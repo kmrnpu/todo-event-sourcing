1434fe2ad18b8dbaf50f3e98284961e8bad12859
dev <dev@kmrnoMac-mini.local>
2025-05-23 11:30:40 +0900

    コンテナ上のアプリケーションにリクエスト可能にする

--- src/index.ts
+++ src/index.ts
@@ -81,7 +81,7 @@ fastify.post("/todos", async (req, reply) => {
   return reply.status(500).send({ error: result.error.message });
 });
 
-fastify.listen({ port: 3000 }, (err, address) => {
+fastify.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
